84ca8afcba3da2cc2797986268f91a735711f1fe
Benjamin Irfan <benjaminirfan.work@gmail.com>
2024-07-12 16:21:16 +0800

    initialised the folders to build an http server using c

--- index.c
+++ index.c
@@ -1,6 +1,14 @@
-#include stdio.h
+#include <fcntl.h>
+#include <stdio.h>
+#include <string.h>
 
-int main() {
-	printf("Hello World");
-	return 0;
+int get_file_descriptor(const char *directory, const char *file_name)
+{
+    char src_file[100];
+    strcpy(src_file, directory);
+    strcat(src_file, file_name);
+
+    int file_descriptor = open(src_file, O_RDONLY);
+
+    return file_descriptor;
 }
